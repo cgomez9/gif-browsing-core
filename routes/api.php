93e6b418b4d4164c8ebf4e6ad26f362a56e2b665
Carlos Gomez <salos_12@hotmail.com>
2019-06-18 14:04:50 -0400

    Authentication controller and routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['json.response']], function () {
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Public routes
+    Route::post('/login', 'Api\AuthController@login')->name('login.api');
+    Route::post('/register', 'Api\AuthController@register')->name('register.api');
+
+    // Private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'Api\AuthController@logout')->name('logout.api');
+    });
+
 });
